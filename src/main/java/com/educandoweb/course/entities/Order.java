c57da3fb45d5e93954b418049745adbff178d123
Carlos Dupont <dupontcam@gmail.com>
2023-04-29 16:21:50 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,19 +16,21 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-    
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
-    
+
     public Order() {
     }
 
@@ -38,27 +42,27 @@ public class Order implements Serializable{
     }
 
     public Long getId() {
-        return id;
+	return id;
     }
 
     public void setId(Long id) {
-        this.id = id;
+	this.id = id;
     }
 
     public Instant getMoment() {
-        return moment;
+	return moment;
     }
 
     public void setMoment(Instant moment) {
-        this.moment = moment;
+	this.moment = moment;
     }
 
     public User getClient() {
-        return client;
+	return client;
     }
 
     public void setClient(User client) {
-        this.client = client;
+	this.client = client;
     }
 
     @Override
